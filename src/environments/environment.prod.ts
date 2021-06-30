62469a5e51411c4d299f4208e2e4a1ce0ab1e563
Yuvaraj <15032282+yuvgeek@users.noreply.github.com>
2021-06-30 20:28:07 +0530

    add environment objects to prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  HARPER_DB_HOST: process.env.HARPER_DB_HOST,
+  HARPER_DB_USERNAME: '',
+  HARPER_DB_PASSWORD: '',
+  firebaseConfig: {},
 };
