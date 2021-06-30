194c8a2f46a95307edd4763ecd92e17ca3ab98a9
Yuvaraj <15032282+yuvgeek@users.noreply.github.com>
2021-07-01 00:08:22 +0530

    minor change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { Router } from '@angular/router';
-import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
